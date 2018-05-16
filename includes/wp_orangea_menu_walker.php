c11165b92468aff656ca28fb1203ed9e916ceb25
Tiafeno <tiafenofnel@gmail.com>
2018-05-16 23:19:05 +0300

    Fix primary menu link

--- includes/wp_orangea_menu_walker.php
+++ includes/wp_orangea_menu_walker.php
@@ -16,9 +16,10 @@ class OG_Primary_Walker extends Walker_Nav_Menu {
 
 
 	function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
+		$post = get_post($item->object_id);
 		$current = empty($output) ? "true" : "false";
-		$output .= sprintf( "\n<li data-current=\"%s\"> <a class=\"anchorage\" href='%s'> %s </a> \n",
-			$current, $item->url, strtoupper($item->title));
+		$output .= sprintf( "\n<li data-current=\"%s\"> <a class=\"anchorage\" href='#%s'> %s </a> \n",
+			$current, $post->post_name, strtoupper($item->title));
 		$this->count++;
 	}
 
--- templates/content-activities.php
+++ templates/content-activities.php
@@ -50,7 +50,7 @@ foreach (array_values($activities_page) as $index => $activitie) :
         <div class="__org_support uk-padding-large uk-margin-large-bottom" uk-grid>
           <div class="uk-width-1-2@m uk-width-1-1">
             <div class="uk-padding-large uk-padding-remove-left uk-padding-remove-top">
-              <h2 id="<?= sanitize_title($activitie->__org_subtitle) ?>" class="ui header" uk-parallax="opacity: 0,1; x: -100, 0; viewport: 0.5">
+              <h2 id="<?= sanitize_title($activitie->post_title) ?>" class="ui header" uk-parallax="opacity: 0,1; x: -100, 0; viewport: 0.5">
                 <?= $activitie->__org_subtitle ?>
                 <div class="sub header uk-margin-small-top">
                   <?= $activitie->post_title ?>
