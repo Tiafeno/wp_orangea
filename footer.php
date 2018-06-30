53d9963d7d6fe9b6b4b67505af5d24fef5f1a45b
Tiafeno <tiafenofnel@gmail.com>
2018-06-30 15:38:35 +0300

    Added falicrea copyright

--- footer.php
+++ footer.php
@@ -10,13 +10,22 @@
 						<footer>
 							<div class="__org_footer">
 								<div class="uk-padding-small" uk-grid>
-									<div class="uk-width-1-6@m uk-width-1-3 uk-margin-medium-left">
+									<div class="uk-width-1-6@m uk-width-1-3">
 										<a href="#logo" title="Nosy Be Communication">
 											<img src="<?= get_template_directory_uri() . '/img/SVG/nb-communication.svg' ?>" class="uk-logo" width="80"
 											     onerror="this.onerror=null; this.src='<?= get_template_directory_uri() . '/img/2x/nb-communication@2x.png' ?>'">
 										</a>
 									</div>
-									<div class="uk-width-5-6@m uk-width-2-3"></div>
+									<div class="uk-width-5-6@m uk-width-2-3">
+										<div class="uk-margin-large-right">
+											<p style="text-align: right; color: #fff;">
+												<a href="http://www.falicrea.com" title="FALICREA" target="_blank"
+												   style="color: #858585; font-size: 9px; font-weight: 700; letter-spacing: 1px">
+													<?= pll_e( "STRATÉGIE, CONCEPTION, MARKETING ET SOUTENU PAR" ) ?> <b>FALICREA</b>
+												</a>
+											</p>
+										</div>
+									</div>
 								</div>
 							</div>
 						</footer>
